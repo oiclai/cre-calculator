72d5a99354bfb1e312efd70f9d8197826df838b9
oiclai <claradealcantara285@gmail.com>
2024-10-15 15:13:45 -0300

    :sparkles: (feat) Adicionando mais recursos

--- CRECalculator.py
+++ CRECalculator.py
@@ -1,26 +1,66 @@
 '''
 Programa para Calculo do CRE
 '''
-cargaGeral=0
-totalSoma=0
-print('''
-       -------------------------------------------------------------------------------
-      | Olá, esse é um programa bem simples com a funcionalidade de calcular o CRE ^^ |
-       -------------------------------------------------------------------------------
+from semester_data.si_schedules import option_first_semester, option_second_semester, option_third_semester, option_fourth_semester, option_fifth_semester, option_sixth_semester
+
+user_cre = 0  # só para declarar a variavel
+
+while True:
+    print('''
+       ----------------------------------------------------------------------------------
+      | Olá, esse programa tem a funcionalidade de calcular o CRE dos alunos de CSTSI ^^ |
+       ----------------------------------------------------------------------------------
+                   Digite '0' em qualquer momento do programa para encerrar
       ''')
-nome=input('> Digite seu nome: ')
-qtdMaterias=int(input('> Quantas matérias foram cursadas: '))
-def calculo (media,carga):
-    global totalSoma, cargaGeral  # declarando variáveis globais
-    materia= media*carga
-    totalSoma+=materia
-    cargaGeral+=carga
-    calculoCRE=totalSoma/cargaGeral
-    return calculoCRE
-# programa principal 
-for i in range (1,qtdMaterias+1):
-    media, carga=map(float, input(f'> Digite a média geral seguida pela carga horaria da {i}ª matéria: ').split())
-    calculoCRE=calculo(media,carga) 
-print('-------------------------------------------------------------------------------')
-print(f'CRE d/{nome}: {calculoCRE:.2f} !')
-print('-------------------------------------------------------------------------------')
+    
+    user_nome=input('> Digite seu nome: ') # -> str
+
+    if user_nome == '0': # -> str
+        break
+    print('''
+      Digite seu semestre dentre as possíveis opções:
+                            (1) - 1º Período (Semestre)
+                            (2) - 2º Período (Semestre)
+                            (3) - 3º Período (Semestre)
+                            (4) - 4º Período (Semestre)
+                            (5) - 5º Período (Semestre)
+                            (6) - 6º Período (Semestre)
+      ''')
+    user_semester=int(input('> ')) # -> int
+    match user_semester:
+            
+        case 1: # -> int
+                option_first_semester()
+                print('-------------------------------------------------------------------------------')
+                print(f'> CRE d/{user_nome}: {user_cre:.2f} !')
+                print('-------------------------------------------------------------------------------')
+        case 2: # -> int
+                option_second_semester()
+                print('-------------------------------------------------------------------------------')
+                print(f'> CRE d/{user_nome}: {user_cre:.2f} !')
+                print('-------------------------------------------------------------------------------')
+        case 3: # -> int
+                option_third_semester()
+                print('-------------------------------------------------------------------------------')
+                print(f'> CRE d/{user_nome}: {user_cre:.2f} !')
+                print('-------------------------------------------------------------------------------')
+        case 4: # -> int
+                option_fourth_semester()
+                print('-------------------------------------------------------------------------------')
+                print(f'> CRE d/{user_nome}: {user_cre:.2f} !')
+                print('-------------------------------------------------------------------------------')
+        case 5: # -> int
+                option_fifth_semester()
+                print('-------------------------------------------------------------------------------')
+                print(f'> CRE d/{user_nome}: {user_cre:.2f} !')
+                print('-------------------------------------------------------------------------------')
+        case 6: # -> int
+                option_sixth_semester()
+                print('-------------------------------------------------------------------------------')
+                print(f'> CRE d/{user_nome}: {user_cre:.2f} !')
+                print('-------------------------------------------------------------------------------')
+        case 0: # -> int
+            break
+        case _:
+            print("Opcao invalida, tente de novo...")
+            continue
---
+++ semester_data/si_schedules.py
@@ -0,0 +1,120 @@
+'''
+FUNÇÕES A SEREM IMPORTADAS PARA CRECALCULATOR.PY
+'''
+
+# ---------------------------------------------------- 1º PERÍODO
+def option_first_semester():
+    global user_cre
+    totalSum=0
+    totalWorkload=0
+    schedule_1semester={
+        'Calculo I': int(83),
+        'APE' : int(83),
+        'Fund. Redes' : int(67),
+        'Linguagem de Marcação' : int(67),
+        'Fund. Computação' : int(50),
+        'Lingua Portuguesa' : int(50)
+    }   
+    for subject in schedule_1semester:
+        media=(int(input(f'> Digite a média geral de {subject} : ')))
+        totalWorkload+=(schedule_1semester[subject])
+        totalSum+=(media*schedule_1semester[subject])
+    user_cre=(totalSum/totalWorkload)
+    return user_cre
+
+
+# ---------------------------------------------------- 2º PERÍODO
+def option_second_semester():
+    global user_cre
+    totalSum=0
+    totalWorkload=0
+    schedule_1semester={
+        'Calculo I': int(83),
+        'APE' : int(83),
+        'Fund. Redes' : int(67),
+        'Linguagem de Marcação' : int(67),
+        'Fund. Computação' : int(50),
+        'Lingua Portuguesa' : int(50)
+    }   
+    for subject in schedule_1semester:
+        media=(int(input(f'> Digite a média geral de {subject} : ')))
+        totalWorkload+=(schedule_1semester[subject])
+        totalSum+=(media*schedule_1semester[subject])
+    user_cre=(totalSum/totalWorkload)
+    return user_cre
+# ---------------------------------------------------- 3º PERÍODO
+def option_third_semester():
+    global user_cre
+    totalSum=0
+    totalWorkload=0
+    schedule_1semester={
+        'Calculo I': int(83),
+        'APE' : int(83),
+        'Fund. Redes' : int(67),
+        'Linguagem de Marcação' : int(67),
+        'Fund. Computação' : int(50),
+        'Lingua Portuguesa' : int(50)
+    }   
+    for subject in schedule_1semester:
+        media=(int(input(f'> Digite a média geral de {subject} : ')))
+        totalWorkload+=(schedule_1semester[subject])
+        totalSum+=(media*schedule_1semester[subject])
+    user_cre=(totalSum/totalWorkload)
+    return user_cre
+# ---------------------------------------------------- 4º PERÍODO
+def option_fourth_semester():
+    global user_cre
+    totalSum=0
+    totalWorkload=0
+    schedule_1semester={
+        'Calculo I': int(83),
+        'APE' : int(83),
+        'Fund. Redes' : int(67),
+        'Linguagem de Marcação' : int(67),
+        'Fund. Computação' : int(50),
+        'Lingua Portuguesa' : int(50)
+    }   
+    for subject in schedule_1semester:
+        media=(int(input(f'> Digite a média geral de {subject} : ')))
+        totalWorkload+=(schedule_1semester[subject])
+        totalSum+=(media*schedule_1semester[subject])
+    user_cre=(totalSum/totalWorkload)
+    return user_cre
+# ---------------------------------------------------- 5º PERÍODO
+def option_fifth_semester():
+    global user_cre
+    totalSum=0
+    totalWorkload=0
+    schedule_1semester={
+        'Calculo I': int(83),
+        'APE' : int(83),
+        'Fund. Redes' : int(67),
+        'Linguagem de Marcação' : int(67),
+        'Fund. Computação' : int(50),
+        'Lingua Portuguesa' : int(50)
+    }   
+    for subject in schedule_1semester:
+        media=(int(input(f'> Digite a média geral de {subject} : ')))
+        totalWorkload+=(schedule_1semester[subject])
+        totalSum+=(media*schedule_1semester[subject])
+    user_cre=(totalSum/totalWorkload)
+    return user_cre
+# ---------------------------------------------------- 6º PERÍODO
+def option_sixth_semester():
+    global user_cre
+    totalSum=0
+    totalWorkload=0
+    schedule_1semester={
+        'Calculo I': int(83),
+        'APE' : int(83),
+        'Fund. Redes' : int(67),
+        'Linguagem de Marcação' : int(67),
+        'Fund. Computação' : int(50),
+        'Lingua Portuguesa' : int(50)
+    }   
+    for subject in schedule_1semester:
+        media=(int(input(f'> Digite a média geral de {subject} : ')))
+        totalWorkload+=(schedule_1semester[subject])
+        totalSum+=(media*schedule_1semester[subject])
+    user_cre=(totalSum/totalWorkload)
+    return user_cre
